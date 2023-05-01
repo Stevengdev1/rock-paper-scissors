81a933e912148e5f6596684aa5650ef85ab9ab6a
Steven <stevendg922@gmail.com>
2023-05-01 13:30:59 -0400

    Completed 'rock-paper-scissors' project.

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
-  const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase(); //toLowerCase() Is used make sure that the user only types in lowercase.  
-   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'fire' ) {
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase(); //toLowerCase() Is used make sure that the user only types in lowercase.
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "fire"
+  ) {
     return userInput;
-  } else { 
-    console.log('Error, please type one the following words: "rock", "paper", "scissors".')
-  } 
+  } else {
+    console.log(
+      'Error, please type one the following words: "rock", "paper", "scissors".'
+    );
+  }
 };
 
 const getComputerChoice = () => {
-  const pickRandomNumber = Math.floor(Math.random()* 3);
+  const pickRandomNumber = Math.floor(Math.random() * 3);
   switch (pickRandomNumber) {
     case 0:
-      return 'rock';
+      return "rock";
     case 1:
-      return 'paper';
+      return "paper";
     case 2:
-      return 'scissors';
+      return "scissors";
   }
 };
-
+getComputerChoice("rock");
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === computerChoice) {
-    return 'Game is tied!'
+    return "Game is tied!";
   }
-  if (userChoice === 'rock') {
-    if (computerChoice === 'paper') {
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
       return "Sorry, computer won!";
     } else {
       return "You Won!";
     }
   }
 
-  if (userChoice === 'paper') {
-    if (computerChoice === 'scissors') {
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
       return "Sorry, computer won!";
     } else {
       return "Congratulations, you won!";
     }
   }
 
-  if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
       return "Sorry, computer won!";
     } else {
       return "Congratulations, you won!";
     }
   }
-  if (userChoice === 'fire') {
-    return 'Congratulations, you won!';
+  if (userChoice === "fire") {
+    return "Congratulations, you won!";
   }
-}
+};
 
 const playGame = () => {
-  const userChoice = getUserChoice('rock'); //If we type the cheat code 'fire' here we will always win!
+  const userChoice = getUserChoice("rock"); //If we type the cheat code 'fire' here we will always win!
   const computerChoice = getComputerChoice();
   console.log(`You pulled: ${userChoice}`);
   console.log(`The computer pulled: ${computerChoice}`);
 
   console.log(determineWinner(userChoice, computerChoice));
-}
-
+};
 
 //This game is played by invoking the function playGame()
-playGame()
+playGame();
 
-
-
-
-
-
-
-
+// Completed rock-paper-scissors project.
